1d6292e25b2341f581033820a2486a91be51c9f1
santi <santi@DESKTOP-LU7UNG7>
2025-12-04 11:41:47 -0500

    Usuario entidad

---
+++ src/main/java/co/uDistrital/avanzada/parcialFinal/usuario/Usuario.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.uDistrital.avanzada.parcialFinal.usuario;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data //De lombok, de esta manera pudiendo usar los get y los set
+@Entity //Le mencionamos que va a ser una tabla
+@Table(name="usuarios") //Nombre de la tabla
+public class Usuario {
+    
+    @Id//Identificador de la tabla
+    @GeneratedValue(strategy=GenerationType.IDENTITY)/*De esta manera asignamos
+    el id y que no sea de manera manual*/
+    private Long id;
+    
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String password;
+    private String ciudad;
+    private String fechaNacimiento;
+    private String generoInteres;
+    private int edadMin;
+    private int edadMax;
+    private String distanciaMax;
+    
+    /**
+     * Constructor vacio
+     * 
+     */
+    public Usuario() {
+    }
+    
+    
+    
+    /*En el constructor no entra el id porque es el identificador de la tabla
+    entonces no puede estar dentro del objeto*/
+    /**
+     * Constructor con los parametros de un usario en tinder
+     * 
+     * @param nombre Nombre del Usuario
+     * @param apellido Apellido del Usuario
+     * @param correo Correo electronico del usuario
+     * @param password Contrasena del usuario
+     * @param ciudad Ciudad del usuario
+     * @param fechaNacimiento Fecha de nacimiento del usuario
+     * @param generoInteres Genero de interes del usuario
+     * @param edadMin Edad minima de preferencias del usuario
+     * @param edadMax Edad maxima de preferencias del usuario
+     * @param distanciaMax Distancia maxima de preferencia del usuario
+     */
+    public Usuario(String nombre, String apellido, String correo,
+            String password, String ciudad, String fechaNacimiento,
+            String generoInteres, int edadMin, int edadMax,
+            String distanciaMax) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.password = password;
+        this.ciudad = ciudad;
+        this.fechaNacimiento = fechaNacimiento;
+        this.generoInteres = generoInteres;
+        this.edadMin = edadMin;
+        this.edadMax = edadMax;
+        this.distanciaMax = distanciaMax;
+    }
+    
+    
+    
+    
+}
---
+++ src/test/java/co/uDistrital/avanzada/parcialFinal/TinderUD/TinderUdApplicationTests.java
@@ -0,0 +1,13 @@
+package co.uDistrital.avanzada.parcialFinal.TinderUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderUdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
